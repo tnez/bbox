e2885d841ac8fca264e1cdf0ae3f7002edf575dd
Travis Nesland <tnesland@gmail.com>
2017-01-20 15:12:35 -0500

    Reset any remaining styling.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>bbox</h2>
-        </div>
       </div>
     );
   }
