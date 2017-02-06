0bd366a2da6b75e67794376952b5f3f2f5acb913
Travis Nesland <tnesland@gmail.com>
2017-02-06 17:21:01 -0500

    Add key handlers to add notes and update initial data

--- src/Player.js
+++ src/Player.js
@@ -11,10 +11,19 @@ class Player extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: this.getInitialData(data),
+      data: data,
       playing: false,
       t: 0,
     }
+  }
+
+  addNote(rowIdx, noteIdx) {
+    const prevData = this.state.data[rowIdx].notes
+    this.setState({data: [
+      ...this.state.data.slice(0, rowIdx),
+      { ...this.state.data[rowIdx], notes: [...prevData.slice(0, noteIdx), true, ...prevData.slice(noteIdx + 1)] },
+      ...this.state.data.slice(rowIdx + 1),
+    ]})
   }
 
   clearData(idx) {
@@ -25,13 +34,23 @@ class Player extends Component {
     ]})
   }
 
-  getInitialData(data) {
-    return data.map(entry => {
-      entry.notes = new Array(NUM_BEATS * NUM_MEASURES).fill(false)
-      // TODO: remove this line
-      entry.notes = entry.notes.map(() => Math.random() > 0.5)
-      return entry
-    })
+  handleKeyDown(e) {
+    switch (e.key) {
+      case 'a':
+        this.addNote(0, this.state.t)
+        break
+      case 's':
+        this.addNote(1, this.state.t)
+        break
+      case 'd':
+        this.addNote(2, this.state.t)
+        break
+      case 'f':
+        this.addNote(3, this.state.t)
+        break
+      default:
+        console.log(`Unhandled Keypress: ${e.key}`)
+    }
   }
 
   tick() {
@@ -42,9 +61,11 @@ class Player extends Component {
   togglePlaying() {
     if (this.state.playing) {
       this.timer && clearInterval(this.timer)
+      document.removeEventListener("keydown", this.handleKeyDown.bind(this))
       this.setState({playing: false})
     } else {
       this.timer = setInterval(this.tick.bind(this), 220)
+      document.addEventListener("keydown", this.handleKeyDown.bind(this))
       this.setState({playing: true})
     }
   }
